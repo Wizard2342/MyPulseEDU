89cc25b1083f1efeebc9e89cd4e2de86d5723df0
Wizard2342 <86030557+Wizard2342@users.noreply.github.com>
2025-07-20 14:04:11 -0400

    Add files via upload
    added bus and index files

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-4 justifyContent-center items-center">
+      <Text className="text-10xl text-dark-200-font-bold"> Pulse EDU?</Text>
+    </View>
+  );
+}
